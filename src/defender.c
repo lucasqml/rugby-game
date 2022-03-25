5130971505f456b0fe47a388bea7af00681e56f8
Lucas Quaresma <lucasqml08@usp.br>
2022-03-25 18:21:23 -0300

    add defender strategy

--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
 
 // Internal headers
 #include "direction.h"
@@ -13,17 +15,54 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                              PRIVATE FUNCTIONS                             */
+/*----------------------------------------------------------------------------*/
+
+direction_t choose_pos_dfd(direction_t directions[])
+{
+  srand((unsigned) time(NULL));
+
+  int pos = rand() % 8;
+
+  return directions[pos];
+}
+
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
+  
   UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  direction_t  directions_dfd[8] = 
+  {
+    (direction_t) DIR_UP,
+    (direction_t) DIR_UP_RIGHT,
+    (direction_t) DIR_RIGHT,
+    (direction_t) DIR_DOWN_RIGHT,
+    (direction_t) DIR_DOWN,
+    (direction_t) DIR_DOWN_LEFT,
+    (direction_t) DIR_UP_LEFT,
+    (direction_t) DIR_STAY
+  };
+
+  static position_t last_position = INVALID_POSITION;
+
+  if(equal_positions(last_position, defender_position) == false)
+  {
+    return (direction_t) DIR_LEFT;
+  }
+  else
+  {
+    return choose_pos_dfd(directions_dfd);
+  }
+
+  last_position = defender_position;
+
 }
 
 /*----------------------------------------------------------------------------*/
